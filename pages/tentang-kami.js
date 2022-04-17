1ac7166fdd96ace9825d274040e8aa11c30e8d6d
unknown <api.sport.team@gmail.com>
2022-04-18 00:34:29 +0700

    tentang kami

--- pages/tentang-kami.js
+++ pages/tentang-kami.js
@@ -6,14 +6,12 @@ export default function tentangKami() {
                     <h2><b>Api Sports Team</b></h2><hr />
                     <h4>Solusi perolahragaan Anda</h4><br />
                     <p>Api Sports dibuat untuk mempermudah pembookingan Anda</p>
+                    <h5><b><icon className='fa fa-instagram' style={{ color: "orange" }} /></b> @api.sport</h5>
+                    <h5 className='mb-2'><b><icon className='fa fa-whatsapp' style={{ color: "orange" }} /></b> 0822-3333-3333  </h5>
                 </div>
                 <div className="col-4 col-sm-4">
                     <img src="./y.png" className="img-fluid" />
                 </div>
-            </div>
-            <div className="row">
-                <h5 className='mb-2'><b><icon className='fa fa-instagram' style={{ color: "orange" }} /></b> @api.sport<b>&nbsp; <icon className='fa fa-whatsapp' style={{ color: "orange" }} /></b> 0333-XXX-XXX</h5>
-                <h5 className='mb-2'></h5>
             </div>
         </div>
     )
